2a56ed9f57ea5e2f03ea32d658f400ad74b7be3b
Marie <marie@exemple.fr>
2022-03-11 15:59:04 +0100

    Begin to adapt test

--- src/main/java/tourGuide/services/RewardsService.java
+++ src/main/java/tourGuide/services/RewardsService.java
@@ -21,7 +21,7 @@ public class RewardsService {
     private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
     //Proximity in miles
     private static final int DEFAUlT_PROXIMITY_BUFFER = 10;;
-    private static final int ATTRACTION_PROXIMITY_RANGE = 10000;
+    private static final int ATTRACTION_PROXIMITY_RANGE = 200;
 
     private int proximityBuffer = DEFAUlT_PROXIMITY_BUFFER;
 
@@ -81,27 +81,6 @@ public class RewardsService {
         }
         return rewards;
     }
-
-/*    *//**
-     * Add new rewards if the user does not have them : to add reward :
-     * <ul><li>the attraction must not have already been take into account</li>
-     * <li>the attraction must be close to a visitedLocation (within the meaning of proximityBuffer) of the user</li></ul>
-     * @param user
-     *//*
-    public void calculateRewards(User user) {
-        List<VisitedLocation> userLocations = user.getVisitedLocations();
-        List<Attraction> attractions = gpsUtil.getAttractions();
-
-        for(VisitedLocation visitedLocation : userLocations) {
-            for(Attraction attraction : attractions) {
-                if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-                    if(nearAttraction(visitedLocation, attraction)) {
-                        user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-                    }
-                }
-            }
-        }
-    }*/
 
     /**
      * Compare the distance, between an attraction and a location, with the ATTRACTION_PROXIMITY_RANGE
--- src/test/java/tourGuide/TestRewardsService.java
+++ src/test/java/tourGuide/TestRewardsService.java
@@ -8,56 +8,54 @@ import java.util.Locale;
 import java.util.UUID;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tourGuide.model.Attraction;
+import tourGuide.model.Location;
+import tourGuide.proxy.GpsProxy;
+import tourGuide.services.RewardsService;
 
+@SpringBootTest
 public class TestRewardsService {
+
+    @Autowired
+    GpsProxy gpsProxy;
 
 	@BeforeClass
 	public static void setup(){
 		Locale.setDefault(Locale.US);
 	}
-
-	/*@Test
-	public void userGetRewards() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(new RewardCentral());
-
-		InternalTestHelper.setInternalUserNumber(0);
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-		
-		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
-		Attraction attraction = gpsUtil.getAttractions().get(0);
-		user.addToVisitedLocations(new VisitedLocation(user.getUserId(), attraction, new Date()));
-		tourGuideService.trackUserLocation(user);
-		List<UserReward> userRewards = user.getUserRewards();
-		tourGuideService.tracker.stopTracking();
-		assertTrue(userRewards.size() == 1);
-	}
 	
 	@Test
-	public void isWithinAttractionProximity() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-		Attraction attraction = gpsUtil.getAttractions().get(0);
+	public void isWithinAttractionProximityTest_WithSameLocation() {
+		RewardsService rewardsService = new RewardsService(gpsProxy);
+		Attraction attraction = gpsProxy.getAttractions().get(0);
 		assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
 	}
-	
-	@Ignore // Needs fixed - can throw ConcurrentModificationException
-	@Test
-	public void nearAllAttractions() {
-		GpsUtil gpsUtil = new GpsUtil();
-		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-		rewardsService.setProximityBuffer(Integer.MAX_VALUE);
 
-		InternalTestHelper.setInternalUserNumber(1);
-		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
-		
-		rewardsService.calculateRewards(tourGuideService.getAllUsers().get(0));
-		List<UserReward> userRewards = tourGuideService.getUserRewards(tourGuideService.getAllUsers().get(0));
-		tourGuideService.tracker.stopTracking();
+    @Test
+    public void isWithinAttractionProximityTest_WithLocationDistant() {
+        RewardsService rewardsService = new RewardsService(gpsProxy);
+        Attraction attraction = gpsProxy.getAttractions().get(0);
+        Location farLocation = new Location(10,10);
+        assertFalse(rewardsService.isWithinAttractionProximity(attraction, farLocation));
+    }
 
-		assertEquals(gpsUtil.getAttractions().size(), userRewards.size());
-	}*/
+    @Test
+    public void isWithinAttractionProximityTest_WithCloseLocation() {
+        RewardsService rewardsService = new RewardsService(gpsProxy);
+        Attraction attraction = gpsProxy.getAttractions().get(0);
+        Location closeLocation = new Location(33, -118);
+        assertTrue(rewardsService.isWithinAttractionProximity(attraction, closeLocation));
+    }
+
+    @Test
+    public void getNearByAttractionsTest(){
+        RewardsService rewardsService = new RewardsService(gpsProxy);
+        List<Attraction> attractions = rewardsService.getNearByAttractions(UUID.randomUUID(), 5);
+        assertEquals(5, attractions.size());
+    }
+
 	
 }
